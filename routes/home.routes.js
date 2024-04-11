581ce83eb254d27d96f157c2bb0ae12d75637c36
Levi <Daz>
2024-04-11 10:46:14 -0600

    no isadmin

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const isAuth = require('../util/is-auth');
-const isAdmin = require('../util/is-admin');
-const isStudent = require('../util/is-student');
 
 const usuariosController = require('../controllers/usuario.controller');
 
