c34cb20c8d95cb134e14f449be892c08470d097d
Levi <Daz>
2024-04-12 20:29:24 -0600

    Botones y search bar

--- models/lista_usuarios.model.js
+++ models/lista_usuarios.model.js
@@ -1,3 +1,29 @@
 const db = require('../util/database');
-const bcrypt = require('bcryptjs');
+
+module.exports = class ListaUsuario {
+    
+    static getVariosRol() {
+        return db.execute(
+            `SELECT u.Nombre, u.Matricula, u.Correo_electronico, u.Beca_actual, Tipo_Rol 
+            FROM usuario u, tiene t, rol r
+            WHERE u.IDUsuario = t.IDUsuario
+            AND t.IDRol = r.IDRol;`
+
+        )
+
+    }
+
+    static getRol() {
+        return db.execute(
+            `SELECT u.Nombre, Tipo_Rol 
+            FROM usuario u, tiene t, rol r
+            WHERE u.IDUsuario = t.IDUsuario
+            AND t.IDRol = r.IDRol;
+            `
+        )
+    }
+
+}
+
+
 
