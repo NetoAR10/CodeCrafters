4b4a6deebeadc1f155642100dbfa19499a7e41ed
Levi <Daz>
2024-04-18 17:19:24 -0600

    cambios antes de pull

--- controllers/historialGeneralPagos.controller.js
+++ controllers/historialGeneralPagos.controller.js
@@ -3,7 +3,13 @@ const HistorialPago = require('../models/historialGeneralPagos.model');
 exports.getHistorialPagosGeneral = async (request, response) => {
     try {
         const [rows] = await HistorialPago.fetchAll();
-        response.render('historialGeneralPagos', { pagos: rows });
+        response.render('historialGeneralPagos', { 
+            pagos: rows, 
+            correo: request.session.correo,
+            permisos: request.session.permisos,
+            rol: request.session.roles,
+            nombre: request.session.nombre,
+        });
     } catch (error) {
         console.error('Error fetching payment history:', error);
         response.status(500).send('Error al obtener el historial de pagos');
