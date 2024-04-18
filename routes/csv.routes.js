1f108aeef800c29123ba3c240400d11a227e8160
Arturo <a01275427@tec.mx>
2024-04-18 01:08:44 -0600

    Solucion error, falta poco para cargar csv, asegurar sesion

--- routes/csv.routes.js
+++ routes/csv.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const isAuth = require('../util/is-auth');
 const csvController = require('../controllers/csv.controller');
 const router = express.Router();
 
-router.get('/upload', csvController.getUploadCsv);
-router.post('/upload', csvController.postUploadCsv);
+router.get('/upload', isAuth, csvController.getUploadCsv);
+router.post('/upload', isAuth, csvController.postUploadCsv);
 
 module.exports = router;
