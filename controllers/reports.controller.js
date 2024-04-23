a80814fc741398393a81c49175cfa73e705838d5
Arturo <a01275427@tec.mx>
2024-04-23 11:16:39 -0600

    Error con las graficas, no se muestran los datos

--- controllers/reports.controller.js
+++ controllers/reports.controller.js
@@ -11,17 +11,16 @@ exports.getReport = async (request, response) => {
     const pagosPorMetodo = await ReportsModel.getPagosPorMetodo();
 
     console.log('Datos para el reporte:', {
-      totalDeudas: totalDeudas[0][0], // Acceso al primer elemento
-      totalPagos: totalPagos[0][0], // Acceso al primer elemento
-      totalDeuda: totalDeuda[0][0], // Acceso al primer elemento
-      totalPago: totalPago[0][0], // Acceso al primer elemento
-      materiasPorCiclo: materiasPorCiclo[0], // Arreglo completo
-      deudaPorConcepto: deudaPorConcepto[0], // Arreglo completo
-      pagosPorMetodo: pagosPorMetodo[0], // Arreglo completo
+      totalDeudas: totalDeudas[0][0], 
+      totalPagos: totalPagos[0][0], 
+      totalDeuda: totalDeuda[0][0], 
+      totalPago: totalPago[0][0], 
+      materiasPorCiclo: materiasPorCiclo[0], 
+      deudaPorConcepto: deudaPorConcepto[0], 
+      pagosPorMetodo: pagosPorMetodo[0], 
     });
 
     response.render('reporte', {
-      // Ahora utiliza los datos corregidos
       totalDeudas: totalDeudas[0][0]?.totalDeudas || 0,
       totalPagos: totalPagos[0][0]?.totalPagos || 0,
       totalDeuda: totalDeuda[0][0]?.totalDeuda || 0,
@@ -29,7 +28,6 @@ exports.getReport = async (request, response) => {
       materiasPorCiclo: materiasPorCiclo[0] || [],
       deudaPorConcepto: deudaPorConcepto[0] || [],
       pagosPorMetodo: pagosPorMetodo[0] || [],
-      // El resto de datos para la vista
       isLoggedIn: request.session.isLoggedIn || false,
       privileges: request.session.privileges || [],
       nombre: request.session.nombre || '',
