31b9498338bef89924acb7b09b0dfe595c6ab162
MasterCrafter69 <r2d2.dany@gmail.com>
2024-04-30 10:10:54 -0600

    conexión BD

--- util/database.js
+++ util/database.js
@@ -1,13 +1,5 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'viadisenio',
-    password: '',
-    port: 3306,
-});
-
-
-
+const pool = mysql.createPool(process.env.JAWSDB_URL)
+  
 module.exports = pool.promise();
