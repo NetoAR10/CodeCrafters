31d1e0dd56249914e1a6adf6f894da12a91a1d4e
Arturo <a01275427@tec.mx>
2024-04-29 18:57:35 -0600

    1 Grafica mas y estilos

--- controllers/reports.controller.js
+++ controllers/reports.controller.js
@@ -6,19 +6,10 @@ exports.getReport = async (request, response) => {
     const totalPagos = await ReportsModel.getTotalPagos();
     const totalDeuda = await ReportsModel.getDeudaTotal();
     const totalPago = await ReportsModel.getPagoTotal();
-    const materiasPorCiclo = await ReportsModel.getMateriasPorCiclo();
     const deudaPorConcepto = await ReportsModel.getDeudaPorConcepto();
     const pagosPorMetodo = await ReportsModel.getPagosPorMetodo();
-
-    console.log('Datos para el reporte:', {
-      totalDeudas: totalDeudas[0][0], 
-      totalPagos: totalPagos[0][0], 
-      totalDeuda: totalDeuda[0][0], 
-      totalPago: totalPago[0][0], 
-      materiasPorCiclo: materiasPorCiclo[0], 
-      deudaPorConcepto: deudaPorConcepto[0], 
-      pagosPorMetodo: pagosPorMetodo[0], 
-    });
+    const materiasPorCiclo = await ReportsModel.getMateriasPorCiclo();
+    const pagosPorMesYMetodo = await ReportsModel.getPagosPorMesYMetodo(); // Asegúrate de tener esta función definida en tu modelo
 
     response.render('reporte', {
       totalDeudas: totalDeudas[0][0]?.totalDeudas || 0,
@@ -28,13 +19,14 @@ exports.getReport = async (request, response) => {
       materiasPorCiclo: materiasPorCiclo[0] || [],
       deudaPorConcepto: deudaPorConcepto[0] || [],
       pagosPorMetodo: pagosPorMetodo[0] || [],
+      pagosPorMesYMetodo: pagosPorMesYMetodo[0] || [], // Envía los datos a la vista
       isLoggedIn: request.session.isLoggedIn || false,
       privileges: request.session.privileges || [],
       nombre: request.session.nombre || '',
       matricula: request.session.matricula || '',
       correo: request.session.correo || '',
       beca: request.session.beca || '',
-      rolUser: request.session.lista?.Tipo_Rol || '', 
+      rolUser: request.session.lista?.Tipo_Rol || '',
       permisos: request.session.permisos || [],
       rol: request.session.roles || '',
       csrfToken: request.csrfToken(),
@@ -44,4 +36,3 @@ exports.getReport = async (request, response) => {
     response.status(500).send('Error interno del servidor.');
   }
 };
-
