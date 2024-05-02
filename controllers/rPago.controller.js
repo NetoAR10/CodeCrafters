cb582c973fa2f0ed46cc7efd166ce74e5cba3747
Levi <Daz>
2024-05-01 18:20:11 -0600

    correcciones redirecciones a un 80%

--- controllers/rPago.controller.js
+++ controllers/rPago.controller.js
@@ -1,4 +1,5 @@
 const pago = require('../models/rPago.model');
+const ListaUsuario = require('../models/pagos_de_alumnos.model');
 
 exports.getRegistrarPago = (request, response, next) => {
     response.render('registrarPago', { 
@@ -6,27 +7,33 @@ exports.getRegistrarPago = (request, response, next) => {
         permisos: request.session.permisos,
         rol: request.session.roles,
         nombre: request.session.nombre,
+        matricula: request.session.matriculaPago,
         csrfToken: request.csrfToken(),
     });
 };
 
-exports.postRegistrarPago = async (req, res, next) => {
-    console.log(req.body);
-    const { IDPago,Referencia, IDDeuda, Cant_pagada, Fecha_de_pago, Metodo, Banco, Nota } = req.body;
+exports.postRegistrarPago = async (request, response, next) => {
+    console.log(request.body);
+    const { IDPago,Referencia, IDDeuda, Cant_pagada, Fecha_de_pago, Metodo, Banco, Nota } = request.body;
     try {
         const nuevoPago = new pago(Referencia, IDDeuda, Cant_pagada, Fecha_de_pago, Metodo, Banco, Nota);
+        const [matricula_table] = await ListaUsuario.getMatricula(request.body.nombre);
+        const matricula = matricula_table[0].Matricula;
+        console.log(matricula);
+
+        request.session.matriculaPago = matricula;
         
         if (IDPago) {
             // Si IDPago, actualizar
             await nuevoPago.update(IDPago);
-            res.redirect('pagos_de_alumnos');
+            response.redirect(`pagos_de_alumnos/deudas/${matricula}`);
         } else {
             // Si no, creamos un nuevo pago
             await nuevoPago.save();
-            res.redirect('pagos_de_alumnos');
+            response.redirect(`pagos_de_alumnos/deudas/${matricula}`);
         }
     } catch (err) {
         console.log(err);
-        res.status(500).send('Error al procesar el pago.');
+        response.status(500).send('Error al procesar el pago.');
     }
 };
--- models/pagos_de_alumnos.model.js
+++ models/pagos_de_alumnos.model.js
@@ -84,5 +84,12 @@ module.exports = class ListaUsuario {
 
    );
    }
+
+   static getMatricula(Nombre){
+    return db.execute(`
+        SELECT usuario.Matricula
+        FROM usuario
+        WHERE usuario.Nombre = ?`,[Nombre]);
+   }
    
 };
